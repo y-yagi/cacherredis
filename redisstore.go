12b280cce6781ee0d693140a788657793b65a1d4
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-08-23 09:43:58 +0900

    First commit

---
+++ redisstore.go
@@ -0,0 +1,42 @@
+package cacherredis
+
+import (
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/y-yagi/cacher"
+)
+
+// WithRedisStore create a new Cache with Redis store.
+func WithRedisStore(opt *redis.Options) *cacher.Cacher {
+	cache := &cacher.Cacher{}
+	client := redis.NewClient(opt)
+	cache.Store = &RedisStore{client: client}
+	return cache
+}
+
+// RedisStore is a type for RedisStore.
+type RedisStore struct {
+	client *redis.Client
+}
+
+// Read cache.
+func (rs *RedisStore) Read(key string) ([]byte, error) {
+	value, err := rs.client.Get(key).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(value), nil
+}
+
+// Write create a new cache.
+func (rs *RedisStore) Write(key string, value []byte, d time.Duration) error {
+	err := rs.client.Set(key, string(value), d).Err()
+	return err
+}
+
+func (rs *RedisStore) Delete(key string) error {
+	_, err := rs.client.Del(key).Result()
+	return err
+}
---
+++ redisstore_test.go
@@ -0,0 +1,53 @@
+package cacherredis
+
+import (
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/y-yagi/cacher"
+)
+
+func TestRedisStore(t *testing.T) {
+	value := []byte("dummy")
+	c := WithRedisStore(redisOpt())
+
+	got, _ := c.Read("cacher-test")
+	if got != nil {
+		t.Fatalf("want nil, got %q", got)
+	}
+
+	c.Write("cacher-test", value, cacher.Forever)
+	got, _ = c.Read("cacher-test")
+	if string(got) != string(value) {
+		t.Fatalf("want %q, got %q", value, got)
+	}
+
+	c.Delete("cacher-test")
+	got, _ = c.Read("cacher-test")
+	if got != nil {
+		t.Fatalf("want nil, got %q", got)
+	}
+}
+
+func TestReidsStoreWithExpired(t *testing.T) {
+	value := []byte("dummy")
+	c := WithRedisStore(redisOpt())
+
+	c.Write("cacher-test", value, 1*time.Second)
+
+	time.Sleep(2 * time.Second)
+
+	got, _ := c.Read("cacher-test")
+	if got != nil {
+		t.Fatalf("want nil, got %q", got)
+	}
+}
+
+func redisOpt() *redis.Options {
+	return &redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	}
+}
